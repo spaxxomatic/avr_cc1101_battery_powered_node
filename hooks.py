f63d6b483bca05d544535bf9718f4aec12490dde
spaxxomatic <a.rachnophob@gmail.com>
2022-06-10 17:02:07 +0200

    PIO update

--- hooks.py
+++ hooks.py
@@ -11,7 +11,7 @@ env.ProcessUnFlags("-DVECT_TAB_ADDR")
 env.Append(CPPDEFINES=("VECT_TAB_ADDR", 0x123456789))
 
 def after_build(source, target, env):
-    print "Copying hex file to target folder"
+    print ("Copying hex file to target folder")
     #if not os.path.isdir("target"):
     #    env.Execute("mkdir target | echo $BUILD_DIR")
     env.Execute("echo ----- copy hex to target folder ---- ")
@@ -19,9 +19,9 @@ def after_build(source, target, env):
 
 def after_build2(source, target, env):
     env.Execute("echo ----- all done ---- ")
-    print "All done"
+    print ("All done")
 
-print "Current build targets", map(str, BUILD_TARGETS)
+print ("Current build targets", map(str, BUILD_TARGETS))
 
 #env.AddPreAction("upload", before_upload)
 #env.AddPostAction("upload", after_upload)
